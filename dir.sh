ce9fbff557834e9c722e77f2ab2e9463ca19e57c
SamuelGYJ <402590400@qq.com>
2019-01-21 22:48:03 +0800

    update version

--- dir.sh
+++ dir.sh
@@ -3,7 +3,7 @@
 # -n 统计数量  类型遍历-type reg  深度遍历-level 2
 # -s 文件大小
 # -i 文件详情
-# -o输出到文件
+# -o输出到文件(暂不支持)
 
 #返回距离root_dir的深度
 function distance(){
@@ -20,23 +20,36 @@ function distance(){
 
 #递归到每个目录下，执行ll 拼接地址 筛选字段
 function info(){
-    echo $1
+    #echo $1
     ls -l $1| awk 'NR>=2'
-    for element in `ls -A`
+    for element in `ls -A $1`
     do
+	#echo $element
         dir_or_file=$1"/"$element
-        if test -d $dir_or_file
+        #echo $dir_or_file
+	if test -d $dir_or_file
         then 
-        # echo $dir_or_file" is dictionary"
+        echo $dir_or_file" is dir"
         {
-            if [ $depth -ge `distance $dir_or_file` ];
+            if [ $2 -eq -1 ]||[ $2 -ge `distance $dir_or_file` ];
                 then
                     #echo $depth
-                    info $dir_or_file
+                    info $dir_or_file $2
             fi  
         }
         fi
     done
+}
+
+function sumRegFile(){
+    if [ $2 -eq -1 ];
+    then
+        regSize=$(du -a $1|awk '{v1="test -f";v2=$2;if(!system(v1" "v2)){sum+=$1}};END{print sum?sum:0}')
+        echo "regular file size: "$regSize" KB"
+    else
+        regSize=$(du -a --max-depth=$2 $1|awk '{v1="test -f";v2=$2;if(!system(v1" "v2)){sum+=$1}};END{print sum?sum:0}')
+        echo "max-depth = "$2" ; regular file size: "$regSize" KB"
+    fi
 }
 
 #$1: base dir
@@ -48,42 +61,43 @@ function getdir(){
         #echo $dir_or_file
         if ([ $element  = ".." ]||[ $element  = "." ])
         then 
-            echo $dir_or_file" is invalid dir"
-        elif test -d $dir_or_file
+            #echo $dir_or_file" is invalid dir"
+            continue 
+	elif test -d $dir_or_file
         then 
             ((dictionary++))
-            echo $dir_or_file" is dictionary"
+            #echo $dir_or_file" is dictionary"
             {
                 # 比较距离root_dir的深度是否小于规定的深度
-                if [ $depth -ge `distance $dir_or_file` ];
+                if [ $2 -eq -1 ]||[ $2 -ge `distance $dir_or_file` ];
                 then
                     #echo $depth
-                    getdir $dir_or_file
+                    getdir $dir_or_file $2
                 fi
             }
         elif test -b $dir_or_file
         then
-            echo $dir_or_file" is block"
+            #echo $dir_or_file" is block"
             ((block++))
         elif test -c $dir_or_file
         then
-            echo $dir_or_file" is char"
+            #echo $dir_or_file" is char"
             ((char++))
         elif test -S $dir_or_file
         then
-            echo $dir_or_file" is socket"
+            #echo $dir_or_file" is socket"
             ((socket++))
         elif test -L $dir_or_file
         then
-            echo $dir_or_file" is link"
+            #echo $dir_or_file" is link"
             ((link++))
         elif test -p $dir_or_file
         then
-            echo $dir_or_file" is pipe"
+            #echo $dir_or_file" is pipe"
             ((pipe++))
         elif test -f $dir_or_file
         then
-            echo $dir_or_file" is regular file"
+            #echo $dir_or_file" is regular file"
             ((reg++))
             fi
     done
@@ -134,15 +148,15 @@ if  [ $1 = '-n' ]
         fi
 
         # echo $depth
-        # echo $root_dir
-        getdir $root_dir
+        # echo $root_dirs
+        getdir $root_dir $depth
         echo "dictionary:" $dictionary
         echo "block:" $block
         echo "char:" $char
         echo "socket:" $socket
         echo "pipe:" $pipe
         echo "link:" $link
-        echo "regular file" $reg
+        echo "regular file:" $reg
     # if 
 #acquire 文件大小
 elif  [ $1 = '-s' ]
@@ -171,7 +185,7 @@ then
         root_dir=$2
         depth=$3
         fi
-    sh sum.sh $2 $3
+    sumRegFile $2 $3
 #acquire 文件详情
 elif [ $1 = '-i' ]
 then 
@@ -199,7 +213,7 @@ then
         root_dir=$2
         depth=$3
         fi
-    info $root_dir
+    info $root_dir $depth
 else
     echo "Illegal input!\n Please retry!"
 
